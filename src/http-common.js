04cb3486d181afa4beec14c60132dcb1a7aab63e
lsquintania <lsquintania@quintania-mac.local>
2020-07-07 20:54:02 -0300

    Adicionado url heroku

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  //baseURL: 'http://localhost:8081/',
+  baseURL: 'https://guarded-wildwood-60613.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
